55b67325ed029b86cee6580eb893d2354585f149
TaeYoonSS <lty0880@gmail.com>
2023-04-04 12:12:27 +0900

    C# 첫번쨰 커밋

---
+++ Day01/ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// Namespace Console App1
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// XML Comment
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Main Entry Point
+        /// </summary>
+        /// <param name="args"> Console Arguments</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C#!!");
+        }
+    }
+}
